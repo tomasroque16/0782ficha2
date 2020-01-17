05cadd9fb25982744a59e29237105ecd0099a2ae
tomasroque16 <tomasmedeirosroque@enta.pt>
2020-01-17 12:35:20 -0100

    ssasa

---
+++ agenciaviagens.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+int main(void);
+int pacotes;
+int pacoteA;
+int pacoteB;
+int PacoteC;
+int tarifa;
+int destinos;
+int Lisboa;
+int Porto;
+int Rabodepeixe;
+int viagens = 620.00;
+int hotel = 40.00;
+int aluguer = 29.00;
+int dias;
+int Europcar;
+int Altis;
+int Carroça;
+int TivoliLisboa;
+int UniversoPorto;
+int Carangueijo;
+
+printf("seja bem vindo a agencia de viagens fraga a:\n->");
+
+ printf("insira o pacote desejado a:\n->");
+ scanf("%d" ,&pacotes)
+
+ printf("insira o numero de dias que deseja o nosso serviço a:\n->");
+ scanf("%d" ,&dias)
+
+do
+{
+ printf("pacotes\n\n1. tarifa\n2. destino\n3. aluguer\n4. alojamento\n5. \n\n\t->");
+ scanf("%d",&choice)
+
+switch (choice)
+ {
+     case 1:
+        printf(" pacote .\n");
+          break;
+          
+     case 2:
+    printf("insira o tipo de pacotes desejado:\n\t->");
+        scanf("%d" ,&pacotes)
+    printf("insira o pacote  :\n\t->");
+        scanf("%d" ,&pacoteA)
+    printf("insira o pacote :\n\t->");
+        scanf("%s" ,&pacoteB)
+    printf("Insira o pacote :\n\t->");
+        scanf("%d" ,&PacoteC)
+
+    case 3:
+    printf("insira o destino desejado :\n\t->"); 
+        scanf("%d" ,&destinos)
+    printf("insira o destino  :\n\t->");
+        scanf("%d" ,&Lisboa)
+    printf("insira o destino  :\n\t->");
+        scanf("%d" ,&Porto)
+    printf("insira o destino  :\n\t->");
+        scanf("%d" ,&Rabodepeixe)
+    
+    case 4:
+    printf("insira o tipo de aluger :\n\t->");
+        scanf("%d" ,&aluguer)
+    printf("Insira o aluguer :\n\t->");
+        scanf("%d" ,&Europcar)
+    printf("Insira o aluguer :\n\t->");
+        scanf("%d" ,&Altis)
+    printf("Insira o aluguer :\n\t->");
+        scanf("%d" ,&Carroça)
+
+    case 5:
+    printf("Insira o alojamento desejado :\n\t->");
+        scanf("%d" ,&hotel)
+    printf("Insira o alojamento :\n\t->");
+        scanf("%d" ,&TivoliLisboa)
+    printf("Insira o alojamento :\n\t->");
+        scanf("%d" ,&UniversoPorto)
+    printf("Insira o alojamento :\n\t->");
+        scanf("%d" ,&Carangueijo)
+
+    case 6:
+    printf("insira ")
---
+++ repositoriaagua.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void);
+ int choice;
+ int entrada;
+ int saida;
+ int valvula;
+ const A;
+ const B;
+
+
+  printf("bem-vindo ao repositorio de agua a:\n->");
+   
+  printf("Deseja abrir a valvula  a:\n->");
+   scanf("%d", &valvula)
+
+  printf(" entrada da agua no repositorio a:\n->");
+   scanf("%d" ,&entrada)
+
+  printf(" saida da agua no repositorio a:\n->");
+   scanf("%d" ,&saida) 
+
+if (const>A && const<B){
+    printf("valvula constantemente fechada:\n->");
+{
+else if (const<A && const>B){
+    printf("Abrir 25% da valvula:\n->");
+
+else if (const<A && const<B){
+    printf(" ligar valvula apenas 50% da mesma :\n->");
+
+else if (const>A && const<B){
+    printf(" ligar valvula igual ao valor saida :\n->");
+
+else if (const>A && const>B){
+    printf(" abrir com 10% a valvula  :\n->");
+
+else if (const>A && const>B){
+    printf(" abrir valvula 100% total  :\n->");
+
+}
+return 0;
